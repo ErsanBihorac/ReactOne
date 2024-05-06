59e1f7083c47dd5c39675686bd6a571c1d2f90d3
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-05-06 18:40:47 +0200

    get data from the inputs

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 class Login extends Component {
+    constructor(props) {
+        super();
+    }
     state = {}
 
+    handleSubmit = (event) => {
+        let email = document.getElementById("email-input-login").value
+        let password = document.getElementById("password-input-login").value
+        event.preventDefault();
+        console.log(email, password);
+        this.props.history.push('/dashboard');
+    }
+
     render() {
-        return <div>
+        return <form onSubmit={this.handleSubmit}>
             <div className="inputs-login">
                 <div className="mb-3">
                     <label htmlFor="email-input-login" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email-input-login" placeholder="Email" />
+                    <input type="text" className="form-control" id="email-input-login" placeholder="Email" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password-input-login" className="form-label">Passwort</label>
-                    <input type="email" className="form-control" id="password-input-login" placeholder="Passwort" />
+                    <input type="text" className="form-control" id="password-input-login" placeholder="Passwort" />
                 </div>
             </div>
             <div className="buttons-login">
-                <button>anmelden</button>
-                <button>registrieren</button>
+                <button type='submit'>anmelden</button>
+                <button ><Link to="/register" className='button'>registrieren</Link></button>
             </div>
-        </div>
+        </form>
     }
 }
 
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
     state = {}
     render() {
-        return <h1>register</h1>
+        return <div>
+            <div className="inputs-login">
+                <div className="mb-3">
+                    <label htmlFor="name-input-login" className="form-label">Name</label>
+                    <input type="email" className="form-control" id="name-input-login" placeholder="Name" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email-input-login" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email-input-login" placeholder="Email" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password-input-login" className="form-label">Passwort</label>
+                    <input type="email" className="form-control" id="password-input-login" placeholder="Passwort" />
+                </div>
+            </div>
+            <div className="buttons-login">
+                <button ><Link to="/" className='button'>registrierung abschließen</Link></button>
+            </div>
+        </div>
     }
 }
 
